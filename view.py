8034907675433b3b89601f7c75360ea307a7dffb
Giuseppe Averta <g.averta3@gmail.com>
2025-03-19 10:37:16 +0100

    fix bgd color

--- view.py
+++ view.py
@@ -3,6 +3,7 @@ import flet as ft
 class View(object):
     def __init__(self, page):
         self._page = page
+        self._page.bgcolor = ft.Colors.AMBER_50
         self._page.title = "TdP 2024 - Indovina il Numero"
         self._page.horizontal_alignment = 'CENTER'
         self._titolo = None
@@ -11,6 +12,8 @@ class View(object):
     def caricaInterfaccia(self):
         self._titolo = ft.Text("Indovina il numero",
                                color="blue", size=24)
+
+        self._page.update()
 
     def setController(self,controller):
         self._controller = controller
